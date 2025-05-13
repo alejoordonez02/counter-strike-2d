b3ee2dab18fc48a102f1af43e5c02263c9c9cadd
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-13 01:02:46 -0300

    feat(server): acceptor - header

---
+++ server/acceptor.h
@@ -0,0 +1,17 @@
+#ifndef ACCEPTOR_H
+#define ACCEPTOR_H
+
+#include "common/socket/socket.h"
+#include "common/thread.h"
+
+class Acceptor: public Thread {
+private:
+    Socket listener;
+
+public:
+    Acceptor(const std::string& servname);
+    void run() override;
+    void stop() override;
+};
+
+#endif
