0d2662a13026743fdd4c8b51620e644138c19f0b
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-13 00:59:35 -0300

    feat: model physics data structures Dirección y posición para la física del modelo

---
+++ common/direction.h
@@ -0,0 +1,9 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+struct Direction {
+    int t;  // t (theta) es el ángulo de la dirección con respecto al eje x
+    Direction(const float& t);
+};
+
+#endif
---
+++ common/position.h
@@ -0,0 +1,10 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+struct Position {
+    float x;
+    float y;
+    Position(const float& x, const float& y);
+};
+
+#endif
