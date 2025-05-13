d917e9fec9d28f735dc413966a975db59d8c5d55
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-13 00:54:55 -0300

    wip(feat): client - initial header

---
+++ client/client.h
@@ -0,0 +1,24 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "common/command.h"
+#include "common/event.h"
+#include "common/queue.h"
+#include "common/receiver.h"
+#include "common/sender.h"
+
+#include "input_handler.h"
+
+class Client {
+private:
+    InputHandler input_handler;
+    Queue<std::unique_ptr<Event>> events;
+    Queue<Command> cmds;
+    Sender sender;
+    Receiver receiver;
+
+public:
+    void run();
+};
+
+#endif
