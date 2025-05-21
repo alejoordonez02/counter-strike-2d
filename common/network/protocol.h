38ef5fc39183cdcf328fc42b2068a3a51bcc5a65
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-21 15:36:42 -0300

    feat(protocol): direction serials

--- common/network/protocol.h
+++ common/network/protocol.h
@@ -8,6 +8,17 @@ constexpr uint8_t MOVE = 0x6D;    // m
 constexpr uint8_t ATTACK = 0x61;  // a
 };  // namespace PlayerCommandSerial
 
+namespace DirectionSerial {
+constexpr uint8_t N = 0x00;
+constexpr uint8_t NE = 0x01;
+constexpr uint8_t E = 0x02;
+constexpr uint8_t SE = 0x03;
+constexpr uint8_t S = 0x04;
+constexpr uint8_t SW = 0x05;
+constexpr uint8_t W = 0x06;
+constexpr uint8_t NW = 0x07;
+};  // namespace DirectionSerial
+
 namespace Message {
 constexpr int LenBytes = 2;  // vamos a mandar algo mas grande que 255 bytes?
 constexpr int MaxLen = 256;
