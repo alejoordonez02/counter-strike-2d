badc3910ea569a647d0c6adbce010845e5eb4953
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-24 18:34:22 -0300

    fix & refactor(deserializer)
    - refactor: use uint8_t vector instead of strings
    - fix attack deserialization
    - feat: deserialize float & deserialize position

--- common/network/deserializer.cpp
+++ common/network/deserializer.cpp
@@ -2,6 +2,7 @@
 
 #include <bit>
 #include <cstdint>
+#include <cstring>
 #include <map>
 #include <stdexcept>
 
@@ -23,18 +24,42 @@ inline const std::map<uint8_t, Direction> srl_to_dir = {
         {DirectionSerial::W, Direction::W}, {DirectionSerial::NW, Direction::NW}};
 };
 
-std::unique_ptr<Move> Deserializer::deserialize_move_command(const std::string& srlzd_cmd) {
+/*
+ * Deserialize float
+ * */
+float Deserializer::deserialize_float(const std::vector<uint8_t>& srlzd_n) {
+    uint32_t dsrlzd_n;
+    std::memcpy(&dsrlzd_n, srlzd_n.data(), sizeof(dsrlzd_n));
+    dsrlzd_n = ntohl(dsrlzd_n);
+    return std::bit_cast<float>(dsrlzd_n);
+}
+
+/*
+ * Deserialize position
+ * */
+Position Deserializer::deserialize_position(const std::vector<uint8_t>& srlzd_pos) {
+    float x = deserialize_float(srlzd_pos);  // first bytes of pos are srlzd_x
+    std::vector<uint8_t> srlzd_y = std::vector<uint8_t>(srlzd_pos.begin() + 4, srlzd_pos.end());
+    float y = deserialize_float(srlzd_y);
+    return Position(x, y);
+}
+
+/*
+ * Deserialize player commands
+ * */
+std::unique_ptr<Move> Deserializer::deserialize_move_command(
+        const std::vector<uint8_t>& srlzd_cmd) {
     return std::make_unique<Move>(srl_to_dir.at(srlzd_cmd[1]));
 }
 
-std::unique_ptr<Attack> Deserializer::deserialize_attack_command(const std::string& srlzd_cmd) {
-    uint32_t srlzd_angle = ntohl(srlzd_cmd[1]);
-    float angle = std::bit_cast<float>(srlzd_angle);
-    Position pos(angle, 2);
+std::unique_ptr<Attack> Deserializer::deserialize_attack_command(
+        const std::vector<uint8_t>& srlzd_cmd) {
+    std::vector<uint8_t> srlzd_pos = std::vector<uint8_t>(srlzd_cmd.begin() + 1, srlzd_cmd.end());
+    auto pos = deserialize_position(srlzd_pos);
     return std::make_unique<Attack>(pos);
 }
 
-std::unique_ptr<Command> Deserializer::deserialize_command(const std::string& srlzd_cmd) {
+std::unique_ptr<Command> Deserializer::deserialize_command(const std::vector<uint8_t>& srlzd_cmd) {
     switch (srlzd_cmd[0]) {
         case PlayerCommandSerial::MOVE:
             return deserialize_move_command(srlzd_cmd);
