3b9bc740767d59b2168c4674bc7f557e58675ecc
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-24 22:25:54 -0300

    feat: map

--- server/model/map.h
+++ server/model/map.h
@@ -1,6 +1,7 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <memory>
 #include <vector>
 
 #include "collidable.h"
@@ -12,8 +13,13 @@
  * nombre: collidable
  * */
 class Map {
+private:
+    std::vector<std::unique_ptr<Collidable>> collidable;
+
 public:
-    std::vector<Collidable> get_collidable();
+    Map(): collidable() {}
+    void add_collidable(std::unique_ptr<Collidable>&& cll) { collidable.push_back(std::move(cll)); }
+    std::vector<std::unique_ptr<Collidable>>& get_collidable() { return collidable; }
 };
 
 #endif
