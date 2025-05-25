b0f3f6bbd9c0e1b93728ee513fbfffbde1cd11e1
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-25 16:47:59 -0300

    feat(position): operator += Direction

--- common/position.h
+++ common/position.h
@@ -1,10 +1,17 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include "direction.h"
 #include "tuple.h"
 
 struct Position: public Tuple<float> {
     Position(const float& x, const float& y): Tuple(x, y) {}
+
+    Position& operator+=(const Direction& dir) {
+        x += dir.x;
+        y += dir.y;
+        return *this;
+    }
 };
 
 #endif
