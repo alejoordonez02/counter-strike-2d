dbb18ebb47f30bae73cee061563d062e6023e641
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-25 16:32:09 -0300

    refactor: position
    - use tuple

--- common/position.h
+++ common/position.h
@@ -1,11 +1,10 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-struct Position {
-    float x;
-    float y;
-    Position(const float& x, const float& y): x(x), y(y) {}
-    bool operator==(const Position& other) const { return x == other.x && y == other.y; }
+#include "tuple.h"
+
+struct Position: public Tuple<float> {
+    Position(const float& x, const float& y): Tuple(x, y) {}
 };
 
 #endif
