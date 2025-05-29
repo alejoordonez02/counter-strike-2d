9d1a6f7d005f71ccefa501549dc5a99308572786
Gonzalo <gonzalobustos97@hotmail.com>
2025-05-29 00:11:02 -0300

    wip: client session

--- server/client_session.h
+++ server/client_session.h
@@ -3,39 +3,42 @@
 
 #include "../common/network/connection.h"
 #include "../common/queue.h"
+#include "../common/network/dto.h"
 #include "../common/network/sender.h"
 #include "../common/network/receiver.h"
 #include "game_monitor.h"
+#include "client_setup.h"
 
 class ClientSession {
 private:
-    Connection conn;
-    Queue<> send_q;
-    Queue<> recv_q;
+    Connection con;
+    Queue<DTO> send_q;
+    Queue<DTO> recv_q;
     Sender sndr;
     Receiver rcvr;
     GameMonitor& game_monitor;
     bool _is_finished;
 
 public:
-    ClientSession(Socket&& s, GameMonitor& gm): conn(std::move(s)), game_monitor(gm), _is_finished(false) {
-        ClientSetup setup(conn, game_monitor);
-        setup.start();
-        setup.join();
+    ClientSession(Socket&& s, GameMonitor& gm): 
+            con(std::move(s)), sndr(con, send_q), rcvr(con, recv_q), game_monitor(gm), _is_finished(false) {
+        // ClientSetup setup(con, game_monitor);
+        // setup.start();
+        // setup.join();
         sndr.start();
         rcvr.start();
     }
 
     bool is_finished() { return _is_finished; }
 
-    void push_game_update() {}
+    void push_game_update(/* std::shared_ptr<GameSnapshot> */) {}
 
     void terminate() {
-        conn.destroy_socket()
-		send_q.close()
-		recv_q.close()
-		sender.join()
-		receiver.join()
+        // con.destroy_socket();
+		send_q.close();
+		recv_q.close();
+		sndr.join();
+		rcvr.join();
         _is_finished = true;
     }
 
