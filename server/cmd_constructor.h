8028e8758ce922bac67f6e8255e13ef81dd54057
Gonzalo <gonzalobustos97@hotmail.com>
2025-05-30 09:50:05 -0300

    feat: CmdConstructor

---
+++ server/cmd_constructor.h
@@ -0,0 +1,56 @@
+#ifndef CMD_CONSTRUCTOR_H
+#define CMD_CONSTRUCTOR_H
+
+#include <functional>
+#include <memory>
+#include <unordered_map>
+#include <cstdint>
+
+#include "player_commands/command.h"
+#include "../common/network/dto.h"
+#include "../common/network/protocol.h"
+
+#include "player_commands/move.h"
+#include "../common/network/dtos/move_dto.h"
+#include "player_commands/attack.h"
+#include "../common/network/dtos/attack_dto.h"
+
+using namespace DTOSerial::PlayerCommands;
+
+using CmdMaker = std::function<std::unique_ptr<Command>(std::unique_ptr<DTO>&&)>;
+
+
+class CmdConstructor {
+private:
+    std::unordered_map<uint8_t, CmdMaker> maker_map;
+
+    CmdConstructor() {
+        maker_map = {
+            {MOVE, [](auto&& dto_p) { return std::make_unique<Move>(dto_p); }},
+            {ATTACK, [](auto&& dto_p) { return std::make_unique<Attack>(dto_p); }},
+            // ...
+        };
+    }
+
+    std::unique_ptr<Command> construct(std::unique_ptr<DTO>&& dto_p) {
+        uint8_t cmd_type = dto_p->get_type();
+
+        if (not maker_map.count(cmd_type))
+            throw std::runtime_error("CmdConstructor error: unknown Command type");
+
+        CmdMaker f = maker_map.at(cmd_type);
+        
+        return f(std::move(dto_p));
+    }
+
+    CmdConstructor(const CmdConstructor&) = delete;
+    CmdConstructor& operator=(const CmdConstructor&) = delete;
+
+    CmdConstructor(CmdConstructor&&) = default;
+    CmdConstructor& operator=(CmdConstructor&&) = default;
+
+    ~CmdConstructor() = default;
+
+};
+
+#endif
