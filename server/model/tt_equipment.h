fb3633be404ac445115c7de76a4dc6bf419ad993
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-05-30 10:12:32 -0300

    feat: weapons
    - separate weapon specializations from weapon.h

---
+++ server/model/tt_equipment.h
@@ -0,0 +1,11 @@
+#ifndef TT_EQUIPMENT_H
+#define TT_EQUIPMENT_H
+
+#include "bomb.h"
+#include "equipment.h"
+
+class TTEquipment: public Equipment {
+    Bomb bomb;
+};
+
+#endif
---
+++ server/model/weapons.h
@@ -0,0 +1,36 @@
+#ifndef WEAPONS_H
+#define WEAPONS_H
+
+#include "weapon.h"
+
+class Fist: public Weapon {
+public:
+    Fist(): Weapon(0, 0, 0, 0, 0) {}
+};
+
+class Knife: public Weapon {
+public:
+    Knife(): Weapon(20, 30, 0.8, 0, 0) {}
+};
+
+class Glock: public Weapon {
+public:
+    Glock(): Weapon(20, 30, 0.8, 30, 700) {}
+};
+
+class Ak47: public Weapon {
+public:
+    Ak47(): Weapon(30, 30, 0.8, 50, 2500) {}
+};
+
+class M3: public Weapon {
+public:
+    M3(): Weapon(20, 30, 0.5, 10, 1200) {}
+};
+
+class Awp: public Weapon {
+public:
+    Awp(): Weapon(100, 30, 1, 100, 3500) {}
+};
+
+#endif
