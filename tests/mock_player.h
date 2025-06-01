dc98e9a7b2d0e1262a368f3143d6504f8b67678d
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-01 11:05:37 -0300

    tests: use custom random generator

--- tests/mock_player.h
+++ tests/mock_player.h
@@ -6,14 +6,25 @@
 #include "../server/model/player.h"
 #include "gmock/gmock.h"
 
+#include "equipment.h"
+#include "weapons.h"
+
 class MockPlayer: public Player {
 private:
     static Position dummy_pos;
     static Map dummy_map;
 
 public:
-    MockPlayer(): Player(dummy_pos, dummy_map) {}
-    MockPlayer(const Position& pos): Player(pos, dummy_map) {}
+    MockPlayer():
+            Player(dummy_pos,
+                   Equipment(std::make_unique<Fist>(), std::make_unique<Fist>(),
+                             std::make_unique<Fist>(), 0),
+                   dummy_map) {}
+    MockPlayer(const Position& pos):
+            Player(pos,
+                   Equipment(std::make_unique<Fist>(), std::make_unique<Fist>(),
+                             std::make_unique<Fist>(), 0),
+                   dummy_map) {}
     MOCK_METHOD(void, move, (const Direction&), (override));
     MOCK_METHOD(void, attack, (const Position&), (override));
     MOCK_METHOD(void, get_attacked, (const int&), (override));
