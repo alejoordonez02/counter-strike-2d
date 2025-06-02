5981c2e180f7081217e5ca158f3fb7b6ebf173e6
Gonzalo <gonzalobustos97@hotmail.com>
2025-06-02 20:53:02 -0300

    feat(Move): Constructor using DTO

--- server/player_commands/move.h
+++ server/player_commands/move.h
@@ -1,9 +1,14 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+#include <memory>
+#include <stdexcept>
+
 #include "command.h"
 #include "../model/player.h"
 #include "../../common/direction.h"
+#include "../../common/network/dto.h"
+#include "../../common/network/dtos/move_dto.h"
 
 /*
  * Move
@@ -14,6 +19,14 @@ private:
 
 public:
     explicit Move(const Direction& d): dir(d) {}
+
+    explicit Move(std::unique_ptr<DTO>&& dto_p) {
+        if (MoveDTO* mov_dto = dynamic_cast<MoveDTO*>(dto_p.get())) {
+            this->dir = std::move(mov_dto->dir);
+        } else {
+            throw std::runtime_error("DTO is not of type MoveDTO");
+        }
+    }
 
     void execute(Player& p) const override { p.move(dir); }
 
