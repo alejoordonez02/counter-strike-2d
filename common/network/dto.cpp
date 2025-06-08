08154553562d066b22e5da8c658742f45554b18a
Gonzalo <gonzalobustos97@hotmail.com>
2025-06-08 07:09:31 -0300

    feat: DTOs Pro Max

--- common/network/dto.cpp
+++ common/network/dto.cpp
@@ -1,26 +1,34 @@
 #include "dto.h"
 
+const std::vector<uint8_t>& DTO::serialize() {
+    if (not _is_serialized) {
+        serialize_into(payload);
+        _is_serialized = true;    
+    }
+    return payload;
+}
+
 // serialization
 
-void DTO::serialize_float(const float& n) {
+void DTO::serialize_float_into(std::vector<uint8_t>& out, const float& n) {
     auto srlzd_n = std::bit_cast<uint32_t>(n);
     srlzd_n = htonl(srlzd_n);
     std::vector<uint8_t> bytes(sizeof(srlzd_n));
     std::memcpy(bytes.data(), &srlzd_n, sizeof(srlzd_n));
-    payload.insert(payload.end(), bytes.begin(), bytes.end());
+    out.insert(out.end(), bytes.begin(), bytes.end());
 }
 
-void DTO::serialize_tuple(const float& x, const float& y) {
-    serialize_float(x);
-    serialize_float(y);
+void DTO::serialize_tuple_into(std::vector<uint8_t>& out, const float& x, const float& y) {
+    serialize_float_into(out, x);
+    serialize_float_into(out, y);
 }
 
-void DTO::serialize_pos(const Position& pos) {
-    serialize_tuple(pos.x, pos.y);
+void DTO::serialize_pos_into(std::vector<uint8_t>& out, const Position& pos) {
+    serialize_tuple_into(out, pos.x, pos.y);
 }
 
-void DTO::serialize_dir(const Direction& dir) {
-    serialize_tuple(dir.x, dir.y);
+void DTO::serialize_dir_into(std::vector<uint8_t>& out, const Direction& dir) {
+    serialize_tuple_into(out, dir.x, dir.y);
 }
 
 
