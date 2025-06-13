ed7265027a38d57099a7d8e1f794f03904abfacf
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-13 07:35:44 -0300

    wip(server): add flavours for testing

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,20 @@
+/* #include "server/server.h"
+#include "tests/client/borrador_server.h" */
 #include "tests/client/mock_server.h"
 
-int main() {
-    mock_server();
+int main(int argc, char* argv[]) {
+    if (argc != 2)
+        return 1;
+
+    std::string servname = argv[1];
+
+    // mock_server(servname);
+
+    MockServer server;
+    server.start(servname);
+
+    /* Server server(servname);
+    server.start(); */
+
     return 0;
 }
