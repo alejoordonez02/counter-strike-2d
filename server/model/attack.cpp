4d264927a6064d883719a18d5edec11ced7cfeed
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-14 05:29:35 -0300

    refactor(attack): decouple from physics

--- server/model/attack.cpp
+++ server/model/attack.cpp
@@ -1,12 +1,14 @@
 #include "server/model/attack.h"
 
-Attack::Attack(PlayerPhysics& physics, Position& pos, Direction& dir,
-               Weapon& weapon, std::weak_ptr<Map> map):
-        physics(physics), pos(pos), dir(dir), weapon(weapon), map(map) {}
+Attack::Attack(Position& pos, Direction& dir, Weapon& weapon,
+               std::vector<std::shared_ptr<Hitbox>>& collidables,
+               std::vector<size_t>& sorted_idx):
+        pos(pos),
+        dir(dir),
+        weapon(weapon),
+        collidables(collidables),
+        sorted_idx(sorted_idx) {}
 
 void Attack::update(float dt) {
-    /*     std::vector<std::shared_ptr<Hitbox>>& get_collidables() { */
-    auto& collidables = map.lock()->get_collidables();
-    auto sorted_idx = physics.get_distance_sorted_collidables_idx(collidables);
     weapon.attack(pos, dir, collidables, sorted_idx);
 }
--- server/model/attack.h
+++ server/model/attack.h
@@ -3,24 +3,37 @@
 
 #include "common/direction.h"
 #include "server/model/action_strategy.h"
-#include "server/model/map.h"
-#include "server/model/player_physics.h"
 #include "server/model/weapon.h"
+
+class Player;
 
 /*
  * Attack
  * */
 class Attack: public ActionStrategy {
 private:
-    const PlayerPhysics& physics;
-    const Position& pos;
-    const Direction& dir;
+    /*
+     * Las opciones acá eran o rediseñar todo para que map se encargue de las
+     * colisiones (en éste punto me doy cuenta de que hubiera sido lo mejor de
+     * un principio, aunque quede menos "elegante" a primera vista), pasar
+     * player physics para que ordene los sorted idx y ref a los collidables
+     * (excluyendo al player), o (lo más lindo): pasar los sorted idx y los
+     * collidables, pero claro, debería estar constantemente actualizando los
+     * sorted idx: costoso... o no, porque lo debería hacer igual en
+     * PlayerPhysics, entonces podría hacerlo en Player, actualizar los sorted
+     * idx y pasar la ref tanto a Attack (o cualquier strategy que lo necesite)
+     * como a PlayerPhysics. Bueno, lo siguiente es el resultado de eso:
+     * */
+    Position& pos;
+    Direction& dir;
     Weapon& weapon;
-    std::weak_ptr<Map> map;
+    std::vector<std::shared_ptr<Hitbox>>& collidables;
+    std::vector<size_t>& sorted_idx;
 
 public:
-    Attack(PlayerPhysics& physics, Position& pos, Direction& dir,
-           Weapon& weapon, std::weak_ptr<Map> map);
+    Attack(Position& pos, Direction& dir, Weapon& weapon,
+           std::vector<std::shared_ptr<Hitbox>>& collidables,
+           std::vector<size_t>& sorted_idx);
     void update(float dt) override;
 };
 
