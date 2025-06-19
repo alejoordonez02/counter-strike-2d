7aa2f4b610548edb5d120e77d081067aeb5b736f
Gonzalo <gonzalobustos97@hotmail.com>
2025-06-19 06:56:08 -0300

    refactor: GameDetailsDTO

--- common/network/dtos/game_details_dto.h
+++ common/network/dtos/game_details_dto.h
@@ -5,20 +5,25 @@
 #include <vector>
 #include <cstdint>
 
-#include "../dto.h"
-#include "../protocol.h"
+#include "common/network/dto.h"
+#include "common/network/protocol.h"
+#include "common/map_name.h"
 
 class GameDetailsDTO: public DTO {
 private:
     std::string name;
-    uint8_t n_players;
-    std::string map_name;
+    uint8_t tt_count;
+    uint8_t ct_count;
+    MapName map_name;
+    bool _is_joinable;
 
     void deserialize() override {
         int i = 1;  // skip 1st byte (DTO type)
         name = deserialize_string(i);
-        n_players = payload[i++];
-        map_name = deserialize_string(i);
+        tt_count = payload[i++];
+        ct_count = payload[i++];
+        map_name = static_cast<MapName>(payload[i++]);
+        _is_joinable = payload[i++];
     }
 
 public:
@@ -26,15 +31,23 @@ public:
         deserialize();
     }
 
-    explicit GameDetailsDTO(const std::string& name, uint8_t n_plys, const std::string& map_name): 
-            DTO(DTOSerial::GAME_DETAILS), name(name), n_players(n_plys), map_name(map_name) {}
+    GameDetailsDTO(const std::string& name, uint8_t tt_cnt, uint8_t ct_cnt, MapName mn, bool is_j): 
+            DTO(DTOSerial::GAME_DETAILS), name(name), tt_count(tt_cnt), ct_count(ct_cnt), map_name(mn), _is_joinable(is_j) {}
 
     void serialize_into(std::vector<uint8_t>& out) override {
         out.push_back(type);
         serialize_string_into(out, name);
-        out.push_back(n_players);
-        serialize_string_into(out, map_name);
+        out.push_back(tt_count);
+        out.push_back(ct_count);
+        out.push_back(static_cast<uint8_t>(map_name));
+        out.push_back(_is_joinable);
     }
+
+    // get_name
+    // get_tt_count
+    // get_ct_count
+    // get_map_name
+    // get_is_joinable
 
     ~GameDetailsDTO() = default;
 };
