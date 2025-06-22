5b4fe9fae398957719e6fea255bcb4a2d9a234e2
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-21 22:01:23 -0300

    feat(world): teams
    en el commit anterior metí sin querer terrorist y ct sin las eses (s)

--- server/game/world/team/counter_terrorists.h
+++ server/game/world/team/counter_terrorists.h
@@ -6,17 +6,9 @@
 class CounterTerrorists: public Team {
 public:
     std::shared_ptr<Player> add_player(
-        int id, const PlayerFactory& player_factory) override {
-        auto p = player_factory.create(id, TeamName::COUNTER_TERRORIST);
-        players.push_back(p);
-        return p;
-    }
+        int id, const PlayerFactory& player_factory) override;
 
-    bool lost_round(const BombState& bomb, const Timer& round_time) override {
-        if (bomb == BombState::EXPLODED) return true;
-        if (bomb == BombState::PLANTED && !players_are_alive()) return true;
-        return false;
-    }
+    bool lost_round(const BombState& bomb, const Timer& round_time) override;
 };
 
 #endif
--- server/game/world/team/team.h
+++ server/game/world/team/team.h
@@ -11,48 +11,27 @@ protected:
 
     Team(std::vector<std::shared_ptr<Player>> players, int won_rounds);
 
-    bool players_are_alive() {
-        bool alive = true;
-        for (auto& p : players) alive &= p->is_alive();
-        return alive;
-    }
+    bool players_are_alive();
 
-    void switch_player_sides() {
-        for (auto& p : players) p->switch_side();
-    }
+    void switch_player_sides();
 
 public:
     Team() = default;
 
-    void update(float dt) {
-        for (auto& p : players) p->update(dt);
-    }
+    void update(float dt);
 
     virtual std::shared_ptr<Player> add_player(
         int id, const PlayerFactory& player_factory) = 0;
 
-    void restart() {
-        for (auto& p : players) p->restart();
-    }
+    void restart();
 
-    int get_won_rounds() { return won_rounds; }
+    int get_won_rounds();
 
     virtual bool lost_round(const BombState& bomb, const Timer& round_time) = 0;
 
-    void switch_sides(Team& other) {
-        switch_player_sides();
-        other.switch_player_sides();
-        auto won_rounds_tmp = std::move(other.won_rounds);
-        auto players_tmp = std::move(other.players);
-        other.won_rounds = std::move(won_rounds);
-        other.players = std::move(players);
-        won_rounds = std::move(won_rounds_tmp);
-        players = std::move(players_tmp);
-    }
+    void switch_sides(Team& other);
 
-    void push_player_data(std::vector<PlayerData>& data) {
-        for (auto& p : players) data.push_back(p->get_data());
-    }
+    void push_player_data(std::vector<PlayerData>& data);
 };
 
 #endif
