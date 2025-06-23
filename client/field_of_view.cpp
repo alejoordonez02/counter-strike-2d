07d55f55dbcd8743a7351e4274bddb077746f4d5
AlePaff <alejandropaff@gmail.com>
2025-06-23 05:07:21 -0100

    feat: fov funcionando pero con el metodo no recomendado el cual era el unico que dieron disponible para hacer el efecto correctamente

--- client/field_of_view.cpp
+++ client/field_of_view.cpp
@@ -12,23 +12,14 @@ FieldOfView::FieldOfView(SDL2pp::Renderer& renderer, uint32_t width, uint32_t he
       width(width), height(height) {
     // inicializa textura negra vacía
     stencil
-        .SetBlendMode(SDL_BLENDMODE_BLEND) // modo mezcla: permite transparencia
-        .SetAlphaMod(240);  // controlar opacidad (alpha) de la textura, va de 0 a 255
+        .SetBlendMode(SDL_BLENDMODE_BLEND); // modo mezcla: permite transparencia
+        // .SetAlphaMod(240);  // controlar opacidad (alpha) de la textura, va de 0 a 255
 }
 
 void FieldOfView::update(SDL2pp::Renderer& renderer) {
     // obtiene la posición del mouse
     int mouse_x, mouse_y;
     SDL_GetMouseState(&mouse_x, &mouse_y);
-    
-    // establecer la textura como target
-    // todo lo que haga de aqui en adelante se dibujará en la textura
-    SDL_SetRenderTarget(renderer.Get(), stencil.Get());
-
-    // limpiar la textura con negro opaco
-    renderer.SetDrawColor(0, 0, 0, 255);
-    // limpia la textura (no borra lo anterior renderizado, ya que se cambió el render target)
-    renderer.Clear();
 
     // centro de la pantalla
     int cx = width / 2;
@@ -52,11 +43,42 @@ void FieldOfView::update(SDL2pp::Renderer& renderer) {
         static_cast<short>(cy + cone_radius * sin(angle + cone_width_angle))
     };
 
+    
+    // establecer la textura como target
+    // todo lo que haga de aqui en adelante se dibujará en la textura
+    SDL_SetRenderTarget(renderer.Get(), stencil.Get());
+
+    // limpiar la textura con negro opaco
+    renderer.SetDrawColor(0, 0, 0, 255);
+    // limpia la textura (no borra lo anterior renderizado, ya que se cambió el render target)
+    renderer.Clear();
+
     // dibuja el cono blanco sobre el fondo negro
     filledPolygonRGBA(renderer.Get(), vx, vy, 3, 255, 255, 255, 255);
 
+    // crear una Surface para aplicar color key (tomar un color como transparente, simil png)
+    SDL2pp::Surface surface_temp(
+        0, width, height, 32, 
+        0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000
+    );
+
+    if (SDL_RenderReadPixels(renderer.Get(), nullptr, SDL_PIXELFORMAT_ARGB8888,
+                             surface_temp.Get()->pixels, surface_temp.Get()->pitch) != 0) {
+        throw std::runtime_error("Failed to read pixels from stencil render target");
+    }
+
+    // aplicar color key al blanco
+    surface_temp.SetColorKey(true, 0xFFFFFF);
+
+    // crear nueva textura desde la Surface
+    stencil = SDL2pp::Texture(renderer, surface_temp);
+
+    // blend mode + alpha mod (transparencia de la textura en general, aplica para el fondo negro)
+    stencil.SetBlendMode(SDL_BLENDMODE_BLEND).SetAlphaMod(220);
+
     // vuelvo a dibujar sobre el target original
     SDL_SetRenderTarget(renderer.Get(), nullptr);
+    renderer.SetDrawColor(0, 0, 0);
 }
 
 void FieldOfView::render(SDL2pp::Renderer& renderer) {
