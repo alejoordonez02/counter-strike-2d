af0bcbc35f80f49c94978877a30042ba3ac37452
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-23 15:32:49 -0300

    refactor(weapon): reloading

--- server/game/world/equipment/weapon.cpp
+++ server/game/world/equipment/weapon.cpp
@@ -7,9 +7,10 @@
 
 Weapon::Weapon(const int& damage, const int& ammo, const float& accuracy,
                const float& range, float fire_rate, const int& cost,
-               const int& ammo_cost):
+               const int& ammo_cost, float reload_time):
     damage(damage), ammo(ammo), accuracy(accuracy), range(range),
-    fire_delay(1 / fire_rate), cost(cost), ammo_cost(ammo_cost) {}
+    fire_delay(1 / fire_rate), cost(cost), ammo_cost(ammo_cost),
+    reload_time(reload_time) {}
 
 void Weapon::attack(Position origin, Direction direction,
                     std::vector<std::shared_ptr<Hitbox>>& collidables,
@@ -33,3 +34,5 @@ void Weapon::attack(Position origin, Direction direction,
 int Weapon::get_cost() const { return cost; }
 
 int Weapon::get_ammo_cost() const { return ammo_cost; }
+
+float Weapon::get_reload_time() { return reload_time; }
--- server/game/world/equipment/weapon.h
+++ server/game/world/equipment/weapon.h
@@ -1,6 +1,7 @@
 #ifndef SERVER_WORLD_EQUIPMENT_WEAPON_H
 #define SERVER_WORLD_EQUIPMENT_WEAPON_H
 
+#include <algorithm>
 #include <memory>
 #include <vector>
 
@@ -11,16 +12,19 @@ class Weapon {
 private:
     int damage;
     int ammo;
+    int max_loaded_ammo;
+    int loaded_ammo;
     float accuracy;
     float range;
     Timer fire_delay;
     int cost;
     int ammo_cost;
+    float reload_time;
 
 public:
     Weapon(const int& damage, const int& ammo, const float& accuracy,
            const float& range, float fire_rate, const int& cost,
-           const int& ammo_cost);
+           const int& ammo_cost, float reload_time);
 
     void update(float dt) { fire_delay.update(dt); }
 
@@ -28,11 +32,18 @@ public:
                         std::vector<std::shared_ptr<Hitbox>>& collidables,
                         const std::vector<size_t>& sorted_idx);
 
-    virtual void load_ammo(const int& count) { ammo += count; }
+    virtual void reload() {
+        int to_load = std::clamp(max_loaded_ammo - loaded_ammo, loaded_ammo,
+                                 max_loaded_ammo);
+        loaded_ammo += to_load;
+        ammo -= to_load;
+    }
 
     virtual int get_cost() const;
 
     virtual int get_ammo_cost() const;
+
+    float get_reload_time();
 
     virtual ~Weapon() = default;
 };
