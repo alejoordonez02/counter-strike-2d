91467892aef0af2bf5b829d4b9d6b9d98382a258
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-23 03:59:13 -0300

    feat(counter terrorist): defuse bomb

--- server/game/world/counter_terrorist.cpp
+++ server/game/world/counter_terrorist.cpp
@@ -1,7 +1,11 @@
 #include "counter_terrorist.h"
 
+#include "actions/defuse_bomb.h"
 #include "ct_equipment.h"
 #include "player.h"
+
+// TODO: make this configurable
+#define TIME_TO_DEFUSE 10
 
 void CounterTerrorist::teleport_to_spawn() { pos = map.lock()->get_ct_spawn(); }
 
@@ -13,4 +17,11 @@ CounterTerrorist::CounterTerrorist(int id, std::weak_ptr<Map> map,
 
 void CounterTerrorist::restart() { respawn(); }
 
-void CounterTerrorist::defuse_bomb() {}
+void CounterTerrorist::defuse_bomb() {
+    auto bomb_state = map.lock()->get_bomb_state();
+    if (bomb_state != BombState::PLANTED) return;
+    auto bomb_pos = map.lock()->get_bomb_position();
+    if (!physics.intersects(bomb_pos)) return;
+
+    action = std::make_unique<DefuseBomb>(pos, map, TIME_TO_DEFUSE);
+}
