c8d414e2b3abacaf6d29b7de96c919265aec5811
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-24 04:18:34 -0300

    fix(common): weapons was missing bomb wpn name

--- common/weapons.h
+++ common/weapons.h
@@ -3,7 +3,7 @@
 
 #include <cstdint>
 
-enum class WeaponName : uint8_t { NONE, KNIFE, GLOCK, AK47, M3, AWP };
+enum class WeaponName : uint8_t { NONE, KNIFE, GLOCK, AK47, M3, AWP, BOMB };
 enum class WeaponType : uint8_t { NONE, PRIMARY, SECONDARY, KNIFE };
 
 #endif
