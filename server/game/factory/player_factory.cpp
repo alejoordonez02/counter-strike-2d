f47eef4387bac54a85618892da3b7e728a59228e
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-24 04:20:28 -0300

    refactor(player factory): welcome plyrs to wpn mkt

--- server/game/factory/player_factory.cpp
+++ server/game/factory/player_factory.cpp
@@ -4,19 +4,35 @@
 #include "server/game/world/terrorist.h"
 
 PlayerFactory::PlayerFactory(std::shared_ptr<Map> map,
-                             const PlayerConfig& config):
-    map(std::move(map)), config(config) {}
+                             const PlayerConfig& config,
+                             std::shared_ptr<WeaponFactory> weapon_factory):
+    map(std::move(map)), config(config), weapon_factory(weapon_factory) {}
 
+/* int id,
+   std::shared_ptr<Weapon> primary,
+   std::shared_ptr<Weapon> secondary,
+   std::shared_ptr<Weapon> knife,
+   std::weak_ptr<Map> map,
+   float max_velocity,
+   float acceleration,
+   float radius,
+   int money,
+   int health,
+   std::shared_ptr<WeaponFactory> weapon_factory); */
 std::shared_ptr<Player> PlayerFactory::create(int id, TeamName team) const {
     switch (team) {
         case TeamName::TERRORIST:
             return std::make_shared<Terrorist>(
-                id, map, config.max_velocity, config.acceleration,
-                config.radius, config.starting_money, config.max_health);
+                id, nullptr, weapon_factory->create(WeaponName::GLOCK),
+                weapon_factory->create(WeaponName::KNIFE), map,
+                config.max_velocity, config.acceleration, config.radius,
+                config.starting_money, config.max_health, weapon_factory);
         case TeamName::COUNTER_TERRORIST:
             return std::make_shared<CounterTerrorist>(
-                id, map, config.max_velocity, config.acceleration,
-                config.radius, config.starting_money, config.max_health);
+                id, nullptr, weapon_factory->create(WeaponName::GLOCK),
+                weapon_factory->create(WeaponName::KNIFE), map,
+                config.max_velocity, config.acceleration, config.radius,
+                config.starting_money, config.max_health, weapon_factory);
         default:
             throw std::runtime_error("Unknown team");
     }
