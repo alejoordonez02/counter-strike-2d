885405d0760e86511f3bf788ae40793fc28fedfa
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-24 00:28:40 -0300

    refactor(server config): weapon

--- server/game/config/weapon_config.h
+++ server/game/config/weapon_config.h
@@ -1,12 +1,18 @@
 #ifndef SERVER_CONFIG_WEAPON_CONFIG_H
 #define SERVER_CONFIG_WEAPON_CONFIG_H
 
+#include "common/weapons.h"
+
 struct WeaponConfig {
-    int damage;
-    int ammo;
+    WeaponType type;
+    float damage;
     float accuracy;
     float range;
+    float bullet_size;
     float fire_rate;
+    float reload_time;
+    int ammo_capacity;
+    int starting_ammo;
     int cost;
     int ammo_cost;
 };
