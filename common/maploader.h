fb1b1da164294e1bd2f2b7f120790a3e1da13118
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-24 09:39:58 -0300

    wip: add todo notes to common map

--- common/maploader.h
+++ common/maploader.h
@@ -5,14 +5,28 @@
 
 #include "common/map_name.h"
 
-struct BlockData{
+/*
+ * TODO: refactorizar:
+ * - usar Position, está en common
+ * - preferir siempre enums antes que strings, por lo menos para el tipo
+ *   de bloque, hay una implementación del mapeo de strings en map_factory.cpp
+ * Ej:
+ * struct StructureConfig {
+ *     StructureType type;
+ *     Position pos;
+ *     int width;
+ *     int height;
+ * };
+ * */
+struct BlockData {
     int x;
     int y;
-    std::string texture;        // NOTE: se utiliza el nombre de la textura, no su path
+    std::string
+        texture;  // NOTE: se utiliza el nombre de la textura, no su path
     std::string type;
 };
 
-struct MapData{
+struct MapData {
     std::string background;
     std::string mapName;
     int width;
@@ -21,18 +35,17 @@ struct MapData{
     std::vector<BlockData> blocks;
 };
 
-class MapLoader
-{
+class MapLoader {
 public:
     MapLoader();
-    
+
     MapData loadMapData(MapName map_name);
     MapData loadMapYaml(const std::string& filePath);
-    
+
     static bool validateMapData(const MapData& data);
 
 private:
     void parseBlocks(const YAML::Node& blocksNode, MapData& data);
 };
 
-#endif // MAPLOADER_H
+#endif  // MAPLOADER_H
