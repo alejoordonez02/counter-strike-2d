ed65ebdf875cc10da179760e67e7a3ede6cf3b01
Alejo Ordoñez <alordonez@fi.uba.ar>
2025-06-24 06:18:49 -0300

    rename: change weapon -> use weapon

--- common/network/dtos/use_weapon_dto.h
+++ common/network/dtos/use_weapon_dto.h
@@ -2,42 +2,41 @@
 #define COMMON_NETWORK_DTOS_CHANGE_WEAPON_DTO_H
 
 #include "common/network/dto.h"
-#include "common/network/protocol.h"
 #include "common/network/dtos/snapshot_dto.h"
+#include "common/network/protocol.h"
 
-class ChangeWeaponDTO: public DTO {
+class UseWeaponDTO: public DTO {
 private:
     // uint8_t weapon_type;
-    EquipmentType weapon_type;
+    WeaponType weapon_type;
 
     // friend class ChangeWeapon;
 
     void deserialize_from(std::vector<uint8_t>::iterator& in) override {
         in++;  // skip 1st byte (DTO type)
-        weapon_type = static_cast<EquipmentType>(*in++);
+        weapon_type = static_cast<WeaponType>(*in++);
     }
 
 public:
-    explicit ChangeWeaponDTO(std::vector<uint8_t>&& bytes): DTO(std::move(bytes)) {
+    explicit UseWeaponDTO(std::vector<uint8_t>&& bytes): DTO(std::move(bytes)) {
         auto payload_it = payload.begin();
         deserialize_from(payload_it);
     }
 
-    explicit ChangeWeaponDTO(std::vector<uint8_t>::iterator& in):
-            DTO(DTOSerial::PlayerCommands::CHANGE_WEAPON) {
+    explicit UseWeaponDTO(std::vector<uint8_t>::iterator& in):
+        DTO(DTOSerial::PlayerCommands::USE_WEAPON) {
         deserialize_from(in);
     }
 
-    explicit ChangeWeaponDTO(EquipmentType w_type):
-            DTO(DTOSerial::PlayerCommands::CHANGE_WEAPON),
-            weapon_type(w_type) {}
+    explicit UseWeaponDTO(WeaponType w_type):
+        DTO(DTOSerial::PlayerCommands::USE_WEAPON), weapon_type(w_type) {}
 
     void serialize_into(std::vector<uint8_t>& out) override {
         out.push_back(type);
         out.push_back(static_cast<uint8_t>(weapon_type));
     }
 
-    ~ChangeWeaponDTO() = default;
+    ~UseWeaponDTO() = default;
 };
 
 #endif
